a8d8f9b1cdd44056e27a4075785c8b4c11c03220
THARINDU_AL2022\Tharindu Gimhana <gimhana329@gmail.com>
2024-10-17 02:49:02 +0530

    footer and nav bar changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+import NavbarPanel from "./components/Navbar/NavbarPanel";
 import Footer from "./components/Footer/Footer";
-
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About } from "./pages/About";
@@ -11,19 +10,24 @@ import { EarthWorkQuantities } from "./pages/EarthWorkQuantities";
 import { MachineModels } from "./pages/MachineModels";
 import { Services } from "./pages/Services";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <Navbar />
-       
+        <NavbarPanel />
+
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/earth-work-quantities" element={<EarthWorkQuantities/>}></Route>
-          <Route path="/machine-models" element={<MachineModels/>}></Route>
-          <Route path="/services" element={<Services/>}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route
+            path="/earth-work-quantities"
+            element={<EarthWorkQuantities />}
+          ></Route>
+          <Route path="/machine-models" element={<MachineModels />}></Route>
+          <Route path="/services" element={<Services />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Footer.css";
+import logo from "../../assets/ProjectPrime_Horizontal_GreenWhite.png";
 
 const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <div>
+      <footer className="bg-dark text-white py-4">
+        <Container>
+          <Row>
+            <Col md={4} className="mb-4">
+              <img src={logo} alt="Logo" />
+              <p>
+                We are a leading company in providing the best services for our
+                clients. Our mission is to deliver quality and innovation.
+              </p>
+            </Col>
+
+            <Col md={4} className="mb-4">
+              <h7>Services</h7>
+              <p>
+                We offer a range of services including web development, mobile
+                apps, and cloud solutions, tailored to your business needs.
+              </p>
+            </Col>
+
+            <Col md={4} className="mb-4">
+              <h7>Contact Us</h7>
+              <Row>
+                <Col xs={6} className="text-start">
+                  <p>Address:</p>
+                  <p>Phone:</p>
+                  <p>Email:</p>
+                </Col>
+
+                <Col xs={6} className="text-end">
+                  <p>123 Business Avenue</p>
+                  <p>+123 456 7890</p>
+                  <p>contact@company.com</p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
